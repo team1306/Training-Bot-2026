00c2149b315742b8f59eb128d730e80315481ae0
Connor Mulligan <cgmulligan33@gmail.com>
2026-07-18 18:53:29 -0500

    fix merge issue

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -9,13 +9,8 @@ public final class BuildConstants {
   public static final String GIT_SHA = "5f29eb7d8d85bc5cb8c52fcd37a8c85099d6cf01";
   public static final String GIT_DATE = "2026-07-18 18:42:48 EDT";
   public static final String GIT_BRANCH = "main";
-<<<<<<< HEAD
   public static final String BUILD_DATE = "2026-07-18 19:51:40 EDT";
   public static final long BUILD_UNIX_TIME = 1784418700536L;
-=======
-  public static final String BUILD_DATE = "2026-07-18 18:46:03 EDT";
-  public static final long BUILD_UNIX_TIME = 1784414763916L;
->>>>>>> f5b6088a7115c25451f2d3d084f90d75ce717db4
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
